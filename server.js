aea219801d5172c0b64f8e0d23849d202a7219a7
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2023-10-18 11:54:42 -0500

    Added POST & user validation to add to animal json file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const { animals } = require('./data/animals.json');
 // setting up the server only takes two steps: we need to instantiate the server, then tell it to listen for requests. 
@@ -51,6 +53,7 @@ function findById(id, animalsArray) {
     return result;
 }
 
+// create route for getting data from server.
 app.get('/api/animals', (req, res) => {
     let results = animals;
     // console.log(req.query)
@@ -60,6 +63,7 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+// create route for getting data from server.
 // 'req' object gives us acces to another property for this case, 'req.params'
 // Unlike the query object, the param object needs to be defined int he route path, with <route>/:<parameterName>
 app.get('/api/animals/:id', (req, res) => {
@@ -69,6 +73,62 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+// Create a function that accepts the POST route's 'req.body' value and the array we want to add the data to. 
+function createNewAnimal(body, animalsArray) {
+    // console.log(body);
+    // our function's main code will go here!
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+
+    // return finished code to post route for response
+    return animal;
+}
+
+// parse incoming string of array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+
+}
+
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        // add animal to json file and animals array in this function
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
+    }
+
+    // req.body is where our incoming content will be
+    // console.log(req.body);
+    // res.json(req.body);
+    
 });
 
 app.listen(PORT, () => {
