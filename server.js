fc17bdabf1d372b51c6d1eab337fb0b6dcd34a08
Roberto Hurtado Jr <rhurtado.267@gmail.com>
2023-10-18 23:39:10 -0500

    .json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-const { animals } = require('./data/animals');
+const { animals } = require('./data/animals.json');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
